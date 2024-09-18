c48a2a04c27577f2b3d83e26d33432682d622d72
jason aghedo <jason.aghedo@gmail.com>
2024-09-18 20:28:28 +0100

    fix: select input styles

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -15,7 +15,7 @@ export const Select = forwardRef<HTMLButtonElement, SelectProps>(
         <SelectPrimitive.Trigger
           ref={forwardedRef}
           className={cn(
-            "inline-flex items-center font-primary justify-center rounded-md px-3 h-10 border bg-[#FFFFFF0A] border-input text-sm text-[#DBE0EB] outline-none w-full",
+            "inline-flex items-center font-primary justify-center rounded-md px-3 h-10 border bg-background border-input text-sm text-[#DBE0EB] outline-none w-full",
             className
           )}
           aria-label="Region"
@@ -33,7 +33,7 @@ export const Select = forwardRef<HTMLButtonElement, SelectProps>(
             position="popper"
             side="bottom"
             sideOffset={5}
-            className="overflow-hidden bg-white rounded-md z-50 w-[var(--radix-select-trigger-width)] max-h-[var(--radix-select-content-available-height)]"
+            className="overflow-hidden bg-background rounded-md z-50 w-[var(--radix-select-trigger-width)] max-h-[200px] border border-input p-2"
           >
             {/* max-h-[var(--radix-select-content-available-height)] */}
             <SelectPrimitive.ScrollUpButton className="flex items-center w-full justify-center h-[25px] bg-white cursor-default">
@@ -55,15 +55,14 @@ Select.displayName = "Select";
 export const SelectItem = forwardRef<
   React.ElementRef<typeof SelectPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
->(({ className, children, ...props }, forwardedRef) => {
+>(({ children, ...props }, forwardedRef) => {
   return (
     <SelectPrimitive.Item
       onClick={(e) => e.stopPropagation()}
       className={cn(
-        "relative flex items-center px-8 py-2 text-sm text-primary font-medium bg-black font-primary border-none",
+        "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm text-primary focus:text-black outline-none focus:bg-accent focus:text-accent-foreground",
         "radix-disabled:opacity-50",
-        "focus:outline-none select-none",
-        className
+        "focus:outline-none"
       )}
       {...props}
       ref={forwardedRef}
